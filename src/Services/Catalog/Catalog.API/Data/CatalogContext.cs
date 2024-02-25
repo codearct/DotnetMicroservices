fb9135c48e553d02f38956afbd933b436a72990c
codearct <codearct@gmail.com>
2024-02-25 16:54:34 +0300

    Created and containerized Catalog API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(
+                configuration
+                .GetValue<string>("DatabaseSettings:ConnectionString")
+                );//connection pool?
+            var database = client
+                .GetDatabase(
+                    configuration
+                    .GetValue<string>("DatabaseSettings:DatabaseName")
+                 );
+
+            Products = database
+                .GetCollection<Product>(
+                    configuration
+                    .GetValue<string>("DatabaseSettings:CollectionName")
+                );//all db tables will come from appsetting and set ctor?
+            CatalogContextSeed.SeedData(Products);//Seed mock data
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿
+global using Catalog.API.Entities;
+global using MongoDB.Driver;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+global using Catalog.API.Data;
+global using Catalog.API.Repositories;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Net;
